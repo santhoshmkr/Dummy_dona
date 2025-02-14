07486da8cbe640f62e558e01eea88c31575c68e3
santhoshmkr <kalidossSanthosh09@gmail.com>
2025-02-14 13:35:56 +0530

    added route

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
-// import { MergedForm } from './pages/Dummy'
-import {MergedForm} from './pages/Dummy'
-import './App.css'
-
-function App() {
-
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { MergedForm } from './pages/Dummy';
+import { NavBar,Home } from './component/Layout';
+import Dashboard from './pages/Dashboard';
+import { Outlet } from 'react-router-dom';
+const AppLayout = () => {
   return (
-    <>
-      <MergedForm/>
-    </>
-  )
-}
+    <div>
+      <NavBar />
+      <Outlet />
+    </div>
+  );
+};
 
-export default App
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<Home />} />
+          <Route path="Manage_form" element={<MergedForm />} />
+          <Route path="Admin" element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/component/Layout.jsx
@@ -0,0 +1,64 @@
+// NavBar.js
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const NavBar = () => {
+  return (
+    <div className="w-full">
+      <div className="flex justify-between items-center px-4 py-4 bg-black text-white">
+        <h1 className="text-lg font-bold">dona</h1>
+        <div className="flex space-x-4">
+          <Link
+            to="/"
+            className="hover:text-gray-300 transition-colors duration-200"
+          >
+            Home
+          </Link>
+          <Link
+            to="/Manage_form"
+            className="hover:text-gray-300 transition-colors duration-200"
+          >
+            Mange forms
+          </Link>
+          <Link
+            to="/Admin"
+            className="hover:text-gray-300 transition-colors duration-200"
+          >
+            Dashboard
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const Home = () => {
+  return <h2>Home Page</h2>;
+};
+
+export const SideBar = () => {
+  return (
+    <div>
+      <div className="flex space-y-4 flex-col">
+        <Link
+          to="/"
+          className="hover:text-gray-300 transition-colors duration-200"
+        >
+          Home
+        </Link>
+        <Link
+          to="/Manage_form"
+          className="hover:text-gray-300 transition-colors duration-200"
+        >
+          Mange forms
+        </Link>
+        <Link
+          to="/Admin"
+          className="hover:text-gray-300 transition-colors duration-200"
+        >
+          Dashboard
+        </Link>
+      </div>
+    </div>
+  );
+};
